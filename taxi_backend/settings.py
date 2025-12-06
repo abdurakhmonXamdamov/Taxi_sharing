ea14a63b014e52fca76453ddb9eaee3bfd5f6175
Abdurakhmon <abdurakhmonxamdamov@gmail.com>
2025-12-06 05:27:28 +0500

    lets finish

--- taxi_backend/settings.py
+++ taxi_backend/settings.py
@@ -231,7 +231,7 @@ else:
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    # SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
